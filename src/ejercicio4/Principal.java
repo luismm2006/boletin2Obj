a7bad351c19f64b25404022f9d5239ee15d8a4a6
luismm2006 <luismulamarquez@gmail.com>
2025-02-25 19:57:46 +0100

    Subida ejercicio 4.

--- src/ejercicio4/Principal.java
+++ src/ejercicio4/Principal.java
@@ -1,5 +1,17 @@
 package ejercicio4;
 
 public class Principal {
+	public static void main(String[] args) {
+		 TarjetaCredito tarjeta = new TarjetaCredito(8, 2500);
+	        tarjeta.mostrarInfo();
 
+	        tarjeta.comprar(500);
+	        tarjeta.comprar(2000);
+	        tarjeta.comprar(500);
+	        tarjeta.comprar(2000);
+	        tarjeta.mostrarInfo();
+
+	        tarjeta.saldarCuenta();
+	        tarjeta.mostrarInfo();
+	}
 }
--- src/ejercicio4/TarjetaCredito.java
+++ src/ejercicio4/TarjetaCredito.java
@@ -2,9 +2,10 @@ package ejercicio4;
 
 public class TarjetaCredito {
 	private int crediticidad;
-	private double gasto;
+	private double gastoLimite;
+	private double gastoAcumulado;
 	
-	public TarjetaCredito(int crediticidad, double gasto) {
+	public TarjetaCredito(int crediticidad, double gastoLimite) {
 		super();
 		if (crediticidad < 1 || crediticidad > 10) {
 			System.out.println("No está dentro de los valores posibles, se asignará el tope de los valores posibles por defecto");
@@ -12,11 +13,11 @@ public class TarjetaCredito {
 		}else {
 			this.crediticidad = crediticidad;
 		}
-		if (gasto > 2000 || gasto < 1) {
+		if (gastoLimite > 2000 || gastoLimite < 1) {
 			System.out.println("No está dentro de los valores posibles, se asignará el tope de los valores posibles por defecto");
-			this.gasto = 2000;
+			this.gastoLimite = 2000;
 		}else {
-			this.gasto = gasto;			
+			this.gastoLimite = gastoLimite;			
 		}
 	}
 
@@ -24,10 +25,39 @@ public class TarjetaCredito {
 		return crediticidad;
 	}
 
-	public double getGasto() {
-		return gasto;
+	public double getGastoLimite() {
+		return gastoLimite;
 	}
 
-	
-	
+	public void comprar(double precio) {
+		if (precio>=1) {
+			gastoAcumulado += precio;
+			if (gastoAcumulado>gastoLimite) {
+				if (crediticidad>=1) {
+					crediticidad --;
+				}
+				if (gastoLimite>=200) {
+					gastoLimite -= 200;
+				}else {
+					gastoLimite = 0;
+				}
+				System.out.println("Has superado el limite!!");
+			}
+		}
+        System.out.println("Compra realizada: " + precio + "€. Gasto total: " + gastoAcumulado + "€.");
+        System.out.println("---------------------------------");
+
+	}
+	public void saldarCuenta() {
+		gastoAcumulado = 0;
+		System.out.println("Tu cuenta está saldada");
+		System.out.println("---------------------------------");
+	}
+	public void mostrarInfo() {
+		System.out.println("--- Información de la Tarjeta ---");
+        System.out.println("Calificación Crediticia: " + crediticidad);
+        System.out.println("Límite de Gasto: " + gastoLimite + "€");
+        System.out.println("Gasto Acumulado: " + gastoAcumulado + "€");
+        System.out.println("---------------------------------");
+	}
 }
